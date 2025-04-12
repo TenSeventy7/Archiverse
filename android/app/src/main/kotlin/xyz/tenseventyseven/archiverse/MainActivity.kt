05180603eb6be19bdbb74ee068c06aea2bcebf83
John Vincent Corcega <git@tenseventyseven.xyz>
2025-04-12 20:54:26 +0800

    android: Enable edge-to-edge for the Android app
    Signed-off-by: John Vincent Corcega <git@tenseventyseven.xyz>

--- android/app/src/main/kotlin/xyz/tenseventyseven/archiverse/MainActivity.kt
+++ android/app/src/main/kotlin/xyz/tenseventyseven/archiverse/MainActivity.kt
@@ -1,5 +1,12 @@
 package xyz.tenseventyseven.archiverse
 
+import androidx.core.view.WindowCompat
+
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+     override fun onPostResume() {
+       super.onPostResume()
+       WindowCompat.setDecorFitsSystemWindows(window, false)
+     }
+}
