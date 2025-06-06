3cc5413b6a9851a6948282d15ce2d6b5f97d07e5
John Vincent Corcega <git@tenseventyseven.xyz>
2025-06-06 20:42:14 +0800

    dialogs/rating_info: Make rating information a full activity
    Signed-off-by: John Vincent Corcega <git@tenseventyseven.xyz>

--- lib/dialogs/rating_info.dart
+++ lib/dialogs/rating_info.dart
@@ -1,48 +1,107 @@
 import 'package:archiverse/components/rating_utils.dart';
 import 'package:archiverse/extensions/context.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
-class _RatingInfoDialog extends StatelessWidget {
+class _RatingInfoActivity extends StatelessWidget {
   final RatingInfo info;
-  const _RatingInfoDialog({required this.info});
+  const _RatingInfoActivity({required this.info});
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(28)),
-      insetPadding: const EdgeInsets.symmetric(horizontal: 16),
-      icon: Container(
-        width: 50,
-        height: 50,
-        constraints: BoxConstraints.tight(const Size(48, 48)),
-        decoration: BoxDecoration(
-          color: info.backgroundColor,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Icon(info.icon, color: info.foregroundColor, size: 32),
-      ),
-      iconPadding: EdgeInsets.only(
-        left: context.screenWidth * 0.5 - 40,
-        right: context.screenWidth * 0.5 - 40,
-        top: 24.0,
-        bottom: 12.0,
-      ),
-      title: Text(info.label),
-      content: Text(
-        info.information,
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: Text(
-            "OK",
-            style: context.textTheme.labelLarge?.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-            ),
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, scrolled) => [_buildAppBar(context)],
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(context.commonPaddingDouble),
+          child: Column(
+            children: [Text(info.information), const SizedBox(height: 48)],
           ),
         ),
+      ),
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        padding: EdgeInsetsGeometry.all(16.0),
+        color: context.theme.colorScheme.surfaceContainer,
+        child: Row(
+          spacing: 16.0,
+          children: [
+            Expanded(
+              child: FilledButton.tonalIcon(
+                onPressed: () => {
+                  // TODO: Implement learn more action
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                icon: Icon(TablerIcons.info_circle),
+                label: Text("Learn more"),
+              ),
+            ),
+            Expanded(
+              child: FilledButton(
+                onPressed: () => Navigator.of(context).pop(),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text("OK"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAppBar(BuildContext context) {
+    return SliverAppBar.large(
+      title: Text(info.label),
+      centerTitle: true,
+      shape: RoundedRectangleBorder(),
+      expandedHeight: 200.0,
+      leading: IconButton(
+        onPressed: () => Navigator.pop(context),
+        icon: Icon(
+          Directionality.of(context) == TextDirection.rtl
+              ? TablerIcons.arrow_right
+              : TablerIcons.arrow_left,
+          size: 22.0,
+        ),
+      ),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: context.commonPaddingDouble,
+          ),
+          child: buildExpandedAppBarWidget(context),
+        ),
+      ),
+    );
+  }
+
+  Widget buildExpandedAppBarWidget(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: context.screenInsets.top + kToolbarHeight),
+        Container(
+          width: 64,
+          height: 64,
+          decoration: BoxDecoration(
+            color: info.backgroundColor,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Icon(info.icon, color: info.foregroundColor, size: 42),
+        ),
+        const SizedBox(height: 16),
+        Text(info.label, style: Theme.of(context).textTheme.headlineSmall),
       ],
     );
   }
@@ -50,9 +109,11 @@ class _RatingInfoDialog extends StatelessWidget {
 
 class RatingInfoDialog {
   static void showSheet(BuildContext context, {required RatingInfo info}) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => _RatingInfoDialog(info: info),
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) => _RatingInfoActivity(info: info),
+        fullscreenDialog: true,
+      ),
     );
   }
 }
