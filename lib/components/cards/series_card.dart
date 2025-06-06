59beced0aaf6f8c18f994a8f62d0767e116c3044
John Vincent Corcega <git@tenseventyseven.xyz>
2025-06-06 20:39:41 +0800

    components/cards/series: Add card for series item
    Signed-off-by: John Vincent Corcega <git@tenseventyseven.xyz>

---
+++ lib/components/cards/series_card.dart
@@ -0,0 +1,28 @@
+import 'package:archiverse/components/cards/base_card.dart';
+import 'package:archiverse/components/items/series_item.dart';
+import 'package:archiverse/models/series.dart';
+import 'package:flutter/material.dart';
+
+class SeriesCard extends BaseCard<Series> {
+  const SeriesCard({
+    super.key,
+    required Series series,
+    this.isSmall = false,
+    super.elevation,
+  }) : super(item: series);
+  const SeriesCard.small({Key? key, required Series series, double? elevation})
+    : this(key: key, series: series, isSmall: true, elevation: elevation);
+
+  final bool isSmall;
+  @override
+  Widget buildContent(BuildContext context) {
+    if (isSmall) {
+      return SeriesItem.small(series: item);
+    }
+
+    return SeriesItem(series: item);
+  }
+
+  // @override
+  // String getRouteName() => AuthorActivity.routeName;
+}
