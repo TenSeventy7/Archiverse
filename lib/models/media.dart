5e5db3469661a6cdfc58e9ac0b47e06257cd9d96
John Vincent Corcega <git@tenseventyseven.xyz>
2025-06-13 20:00:20 +0800

    models/media: Add localized name and media icon
    Signed-off-by: John Vincent Corcega <git@tenseventyseven.xyz>

--- lib/models/media.dart
+++ lib/models/media.dart
@@ -2,6 +2,7 @@
 
 import 'package:archiverse/extensions/context.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
 enum Media {
   ANIME_MANGA,
@@ -72,32 +73,59 @@ enum Media {
     }
   }
 
-  // String toLocalName(BuildContext context) {
-  //   var localizations = context.strings;
-  //   switch (this) {
-  //     case Media.ANIME_MANGA:
-  //       return localizations.fandom_anime_manga;
-  //     case Media.BOOKS_LITERATURE:
-  //       return localizations.fandom_books_literature;
-  //     case Media.CARTOONS_COMICS_GRAPHIC_NOVELS:
-  //       return localizations.fandom_cartoons_comics_graphic_novels;
-  //     case Media.CELEBRITIES_REAL_PEOPLE:
-  //       return localizations.fandom_celebrities_real_people;
-  //     case Media.MOVIES:
-  //       return localizations.fandom_movies;
-  //     case Media.MUSIC_BANDS:
-  //       return localizations.fandom_music_bands;
-  //     case Media.OTHER_MEDIA:
-  //       return localizations.fandom_other_media;
-  //     case Media.THEATER:
-  //       return localizations.fandom_theater;
-  //     case Media.TV_SHOWS:
-  //       return localizations.fandom_tv_shows;
-  //     case Media.VIDEO_GAMES:
-  //       return localizations.fandom_video_games;
-  //     case Media.UNCATEGORIZED:
-  //     default:
-  //       return localizations.fandom_uncategorized;
-  //   }
-  // }
+  IconData get icon {
+    switch (this) {
+      case Media.ANIME_MANGA:
+        return TablerIcons.torii;
+      case Media.BOOKS_LITERATURE:
+        return TablerIcons.book;
+      case Media.CARTOONS_COMICS_GRAPHIC_NOVELS:
+        return TablerIcons.mickey;
+      case Media.CELEBRITIES_REAL_PEOPLE:
+        return TablerIcons.user;
+      case Media.MOVIES:
+        return TablerIcons.movie;
+      case Media.MUSIC_BANDS:
+        return TablerIcons.piano;
+      case Media.OTHER_MEDIA:
+        return TablerIcons.folders;
+      case Media.THEATER:
+        return TablerIcons.masks_theater;
+      case Media.TV_SHOWS:
+        return TablerIcons.device_tv;
+      case Media.VIDEO_GAMES:
+        return TablerIcons.device_gamepad;
+      case Media.UNCATEGORIZED:
+        return TablerIcons.category;
+    }
+  }
+
+  String toLocalName(BuildContext context) {
+    var localizations = context.strings;
+    switch (this) {
+      case Media.ANIME_MANGA:
+        return localizations.fandom_anime_manga;
+      case Media.BOOKS_LITERATURE:
+        return localizations.fandom_books_literature;
+      case Media.CARTOONS_COMICS_GRAPHIC_NOVELS:
+        return localizations.fandom_cartoons_comics_graphic_novels;
+      case Media.CELEBRITIES_REAL_PEOPLE:
+        return localizations.fandom_celebrities_real_people;
+      case Media.MOVIES:
+        return localizations.fandom_movies;
+      case Media.MUSIC_BANDS:
+        return localizations.fandom_music_bands;
+      case Media.OTHER_MEDIA:
+        return localizations.fandom_other_media;
+      case Media.THEATER:
+        return localizations.fandom_theater;
+      case Media.TV_SHOWS:
+        return localizations.fandom_tv_shows;
+      case Media.VIDEO_GAMES:
+        return localizations.fandom_video_games;
+      case Media.UNCATEGORIZED:
+      default:
+        return localizations.fandom_uncategorized;
+    }
+  }
 }
